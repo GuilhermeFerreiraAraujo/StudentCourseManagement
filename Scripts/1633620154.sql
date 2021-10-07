ba42a86ff9d08f8001548aaab66c006e5d7a7172
MASTER\Master <guilherme.ferreira28@outlook.com>
2021-10-07 16:25:18 +0100

    ADD Create database script

---
+++ Scripts/1633620154.sql
@@ -0,0 +1,43 @@
+
+IF DB_ID('StudentCourseManagement') IS NULL
+BEGIN
+	CREATE DATABASE StudentCourseManagement
+END
+GO
+
+USE StudentCourseManagement
+Go
+
+IF NOT (EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo' 
+                 AND  TABLE_NAME = 'Courses'))
+BEGIN
+    CREATE TABLE Courses(
+		Id INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+		Name NVARCHAR(100) NOT NULL,
+		Code NVARCHAR(500) NOT NULL,
+		Teacher NVARCHAR(500) NOT NULL,
+		StartDate SMALLDATETIME NOT NULL,
+		EndDate SMALLDATETIME NOT NULL
+	);
+END
+GO
+
+IF NOT(EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo' 
+                 AND  TABLE_NAME = 'Students'))
+BEGIN
+    CREATE TABLE Students(
+		Id INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+		FirstName NVARCHAR(500) NOT NULL,
+		Surname NVARCHAR(500) NOT NULL,
+		Gender NVARCHAR(10) NOT NULL,
+		DateOfBirth SMALLDATETIME NOT NULL,
+		Address1 NVARCHAR(200) NULL,
+		Address2 NVARCHAR(200) NULL,
+		Address3 NVARCHAR(200) NULL
+	);
+END
+GO
