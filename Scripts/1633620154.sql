1a61ddaa3d576913de226d5fffa06ad241eebad3
MASTER\Master <guilherme.ferreira28@outlook.com>
2021-10-10 12:12:05 +0100

    ADD Store procedures

--- Scripts/1633620154.sql
+++ Scripts/1633620154.sql
@@ -57,3 +57,77 @@ BEGIN
 END
 GO
 
+
+
+IF NOT EXISTS(SELECT 1 FROM sys.procedures
+			WHERE Name = 'GetStudentsWithoutAllSubscriptions')
+BEGIN
+		EXEC ('
+		
+				CREATE PROCEDURE GetStudentsWithoutAllSubscriptions @MaxNumberOfSubscriptions int
+			AS 
+					WITH GroupedSubscriptions(IdStudent, Subscriptions) as (
+						select IdStudent, count(IdCourse) as Subscriptions
+						from Subscriptions
+						group by IdStudent
+						)
+					SELECT count(*) as AmountOfStudentsWithoutAllSubscriptions
+					FROM GroupedSubscriptions 
+					WHERE Subscriptions < @MaxNumberOfSubscriptions
+		')
+END
+GO
+
+IF NOT EXISTS(SELECT 1 FROM sys.procedures
+			WHERE Name = 'GetCoursesWithoutFullCapacity')
+BEGIN
+		EXEC ('
+		
+				CREATE PROCEDURE GetCoursesWithoutFullCapacity @MaxNumberOfSubscriptions int
+			AS 
+					WITH GroupedSubscriptions(IdCourse, Subscriptions) as (
+						select IdCourse, count(IdStudent) as Subscriptions
+						from Subscriptions
+						group by IdCourse
+						)
+					SELECT count(*) as AmountOfCoursesWithoutFullCapacity
+					FROM GroupedSubscriptions 
+					WHERE Subscriptions < @MaxNumberOfSubscriptions
+		')
+END
+GO
+
+
+
+
+IF NOT EXISTS(SELECT 1 FROM sys.procedures
+			WHERE Name = 'GetCoursesByStudentId')
+BEGIN
+		EXEC ('
+				CREATE PROCEDURE GetCoursesByStudentId @StudentId int
+				AS 
+					SELECT *
+					FROM Courses
+					WHERE Id in (
+						select idCourse 
+						from Subscriptions
+						where IdStudent = @StudentId)
+						')
+END
+GO
+
+IF NOT EXISTS(SELECT 1 FROM sys.procedures
+			WHERE Name = 'GetStudentsByCourseId')
+BEGIN
+		EXEC ('
+				CREATE PROCEDURE GetStudentsByCourseId @CourseId int
+				AS 
+					SELECT *
+					FROM Students
+					WHERE Id in (
+						select idStudent 
+						from Subscriptions
+						where IdCourse = @CourseId)
+						')
+END
+GO
